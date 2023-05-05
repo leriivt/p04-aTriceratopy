94f4daf22a7fc5338533b13973e060c18e500159
leriivt <verit.li@gmail.com>
2023-05-05 13:11:56 -0400

    minor

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('map.html')
 
+@app.route('/summary')
+def summary():
+  return "nop"
+
 
 if __name__ == '__main__':
   app.debug = True
