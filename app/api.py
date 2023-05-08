51a40f73284df6b805b71cee3c83864071d807c1
Prattay <pdey30@stuy.edu>
2023-05-08 13:10:56 -0400

    upd

--- app/api.py
+++ app/api.py
@@ -6,11 +6,15 @@ def get_key():
         key = file.read()
     return key
 
+def initialize():
+    return requests.get(f"http://api.positionstack.com/v1/forward?access_key={get_key()}")
+
 def forward_geocode(address):
-    return requests.get(f"https://api.positionstack.com/v1/forward?access_key={get_key()}&query={address}")
+    return requests.get(f"http://api.positionstack.com/v1/forward?access_key={get_key()}&query={address}")
 
 
 def reverse_geocode(latitude, longitude):
-    return requests.get(f"https://api.positionstack.com/v1/reverse?access_key={get_key()}&query={latitude},{longitude}")
+    return requests.get(f"http://api.positionstack.com/v1/reverse?access_key={get_key()}&query={latitude},{longitude}")
 
-print( forward_geocode("1600 Pennsylvania Ave NW, Washington DC"))    
+# 
+# print( forward_geocode("1600 Pennsylvania Ave NW, Washington DC"))    
