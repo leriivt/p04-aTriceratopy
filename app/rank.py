ce5ab8af6cf21e7cd8262d2636c6a9b2eb520b35
leriivt <verit.li@gmail.com>
2023-05-16 13:12:32 -0400

    minor

---
+++ app/rank.py
@@ -0,0 +1,9 @@
+from db import *
+
+#these functions return strings in csv layout for graphs in d3.js
+def blank_by_time(category):
+    return null
+
+#create a dict with all operators
+def blank_by_operator(category):
+    return null
