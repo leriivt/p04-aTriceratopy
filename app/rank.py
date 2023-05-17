3dc4843234d8cd7e2454faca7bd324406c189640
Prattay <pdey30@stuy.edu>
2023-05-17 12:44:42 -0400

    upd

--- app/rank.py
+++ app/rank.py
@@ -1,8 +1,11 @@
 from db import *
 
 #these functions return strings in csv layout for graphs in d3.js
-def blank_by_time(category):
-    return null
+def blank_by_date(category):
+    csv_string = ""
+    
+
+    
 
 #create a dict with all operators
 def blank_by_operator(category):
