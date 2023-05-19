34ea69793355f6f3a5e19fa11ad8cd33b2b6e788
leriivt <verit.li@gmail.com>
2023-05-19 13:13:04 -0400

    api

--- app/api.py
+++ app/api.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-def get_key():
-    with open('../key_positionstack.txt', 'r') as file:
+def get_key_bing():
+    with open('./keys/key_bingMaps.txt', 'r') as file:
         key = file.read()
     return key
 
@@ -12,12 +12,16 @@ def get_key_mp():
     return key
 
     
+#returns longitude and latitude in a string
+def forward_geocode(location):
+    res = requests.get(f"http://dev.virtualearth.net/REST/v1/Locations?query={location}&key={get_key_bing()}")
+    json = res.json()
+    #print(json)
+    return json["resourceSets"][0]["resources"][0]["point"]["coordinates"]
 
-def forward_geocode(address):
-    return requests.get(f"http://api.positionstack.com/v1/forward?access_key={get_key()}&query={address}")
-
-
+print(forward_geocode("New York, New York"))
+'''
 def reverse_geocode(latitude, longitude):
     return requests.get(f"http://api.positionstack.com/v1/reverse?access_key={get_key()}&query={latitude},{longitude}")
-
+'''
 # print( forward_geocode("1600 Pennsylvania Ave NW, Washington DC"))    
